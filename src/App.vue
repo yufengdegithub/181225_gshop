70655e4a04e9ca10df0f3b716a59bffe222eba43
fish <yufengxiaoyuer@126.com>
2018-12-25 22:29:26 +0800

    gshop

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script src="../../../尚硅谷Vue硅谷外卖项目视频/code/gshop-server_final/bin/www"></script>
+<template>
+    <div id="app">
+        <router-view/>
+        <FooterGuide v-show="$route.meta.showFooter"/>
+    </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+  export default {
+    components:{
+      FooterGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    width 100%
+    height:100%
+    background #f5f5f5
+</style>
